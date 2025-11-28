4b7c39590b76a2d50593871e7908883d528ff463
OmniXP.Studio <contact@omnixp.studio>
2025-11-28 15:23:25 +0000

    Add Google Tag

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -5,6 +5,17 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/logo-footer.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K37FJ1P6V5" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-K37FJ1P6V5');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
